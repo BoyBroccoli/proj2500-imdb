c8e9f4ddad7a2cf6ba88956c62b63c02ec6bc0e2
redshinigami83 <97739678+redshinigami83@users.noreply.github.com>
2023-12-05 16:21:37 -0400

    - Initial programming for actor page - Initial draft of actor page xaml

--- prog2500-imdb/Pages/ActorPage.xaml.cs
+++ prog2500-imdb/Pages/ActorPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using IMDB.Data;
+using IMDB.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +24,65 @@ namespace prog2500_imdb.Pages
     /// </summary>
     public partial class ActorPage : Page
     {
+        ImdbContext _context = new ImdbContext();
+        CollectionViewSource ActorViewSource = new CollectionViewSource();
+
         public ActorPage()
         {
             InitializeComponent();
+
+            // Link markup source to view source
+            ActorViewSource = (CollectionViewSource)FindResource(nameof(ActorViewSource));
+
+            // Load table from source
+            _context.Names.Load();
+            _context.Titles.Load();
+
+            // Set View Source to use table
+            ActorViewSource.Source = _context.Names.Local.ToObservableCollection();
+        }
+
+        private void Search_Button_Click(object sender, RoutedEventArgs e)
+        {
+            ActorListView_Load(Search_Field.Text.ToLower());
+        }
+
+        private void ActorListView_Load(string Search_Term)
+        {
+            ActorListView.Items.Clear();
+
+            IQueryable query;
+
+            if(Search_Term is not null)
+            {
+                query =
+                    from actor in _context.Names
+                    where actor.PrimaryName.ToLower().Contains(Search_Term)
+                    group actor by actor.PrimaryName.ToUpper().Substring(0, 1) into actor_group
+                    select new
+                    {
+                        actor_index = actor_group.Key,
+                        actor_count = actor_group.Count().ToString(),
+                        actor = actor_group.ToList<Name>()
+                    };
+            }
+            else
+            {
+                query =
+                    from actor in _context.Names
+                    group actor by actor.PrimaryName.ToUpper().Substring(0, 1) into actor_group
+                    select new
+                    {
+                        actor_index = actor_group.Key,
+                        actor_count = actor_group.Count().ToString(),
+                        actor = actor_group.ToList<Name>()
+                    };
+            }            
+
+            foreach(var actor in query)
+            {
+                ActorListView.Items.Add(actor);
+            }
         }
     }
 }
