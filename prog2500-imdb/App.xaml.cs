c464f514fb74bc511829f6d7e090eb42eaa44d7d
Savag <Savag@DESKTOP-2494BLH>
2023-12-05 09:36:17 -0400

    Add project files.

---
+++ prog2500-imdb/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace prog2500_imdb
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ prog2500-imdb/Models/Generated/Rating.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace IMDB.Models;
+
+public partial class Rating
+{
+    [Key]
+    [Column("titleID")]
+    [StringLength(10)]
+    [Unicode(false)]
+    public string TitleId { get; set; } = null!;
+
+    [Column("averageRating", TypeName = "numeric(4, 2)")]
+    public decimal? AverageRating { get; set; }
+
+    [Column("numVotes")]
+    public int? NumVotes { get; set; }
+
+    [ForeignKey("TitleId")]
+    [InverseProperty("Rating")]
+    public virtual Title Title { get; set; } = null!;
+}
